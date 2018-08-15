c377a6b142a5e3c443978330eca4f5c3313b903b
John Rodriguez <john.rodriguez.25@hotmail.com>
2018-08-14 23:02:15 -0500

    Crear ramas

--- routes/api/nodes.js
+++ routes/api/nodes.js
@@ -4,13 +4,29 @@ var nodeModule = require('../../models/node');
 
 router.get('/showall', function(req, res, next) {
     let node = new nodeModule();
+
     node.name = 'Node 1';
     node.value = '80';
     return res.json(node);
 });
 
 router.post('/create', function(req, res, next) {
-    return res.json(req.body.user)
+    let treeData = req.body.treeData;
+    let node1 = req.body.node1;
+    let node2 = req.body.node2;
+    let branchNode1 = {};
+    let branchNode2 = {};
+
+    
+    branchNode1 = treeData.find(item => {
+        return item.values.some(_item => _item == node1)
+    });
+
+    branchNode2 = treeData.find(item => {
+        return item.values.some(_item => _item == node2)
+    });    
+
+    return res.json([branchNode1, branchNode2]);
 });
 
 module.exports = router;
